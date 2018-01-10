f80cc03c39f7fa7f69e4f0571598b69031edc30c
Nebz <28622481+NebzHB@users.noreply.github.com>
2018-01-10 15:04:12 +0100

    Update timedData.js

--- timedData.js
+++ timedData.js
@@ -1,104 +1,112 @@
 'use strict';
 
-module.exports = function() {
+module.exports = {
+	timedData: timedData
+};
 
-	class timedData {
-		constructor(params) {
-			this.subscribedServices = [];
-			this.minutes    = params.minutes || 10;
-			this.initialPush= params.initialPush || false;
-			this.intervalID = null;
-			this.running    = false;
-			this.lastValue  = params.lastValue;
-			this.callback   = (typeof(params.callback)=='function')?params.callback.bind(this):undefined;
+function timedData(params) {
 
-			this.history	= [];
-			
-			if(this.initialPush) {
-				setImmediate(this.historicalDatas.bind(this));
-			}
-			this.start();
-		}
+		this.subscribedServices = [];
+		this.minutes    = params && params.minutes || 10;
+		this.initialPush= params && params.initialPush || false;
+		this.intervalID = null;
+		this.running    = false;
+		this.lastValue  = params && params.lastValue || 0;
+		this.callback   = (params && typeof(params.callback)=='function')?params.callback.bind(this):undefined;
+
+		this.history	= [];
 		
-		// Subscription management
-		subscribe(service,callback) {
-            if(typeof(callback) == 'function') {
-                callback = callback.bind(this);
-            }
-            
-			this.subscribedServices.push({
-				'service'   : service,
-				'callback'  : callback,
-				'history'   : []
-			});
-			if(!this.running) this.start();
+		if(this.initialPush) {
+			setImmediate(this.historicalDatas.bind(this));
 		}
-		getSubscriber(service) {
-			let findServ = function(element) {
-				return element.service === service;
-			};
-			return this.subscribedServices.find(findServ);
-		}
-		_getSubscriberIndex(service) {
-			let findServ = function(element) {
-				return element.service === service;
-			};
-			return this.subscribedServices.findIndex(findServ);
-		}
-		getSubscribers() {
-			return this.subscribedServices;
-		}
-		unsubscribe(service) {
-			let index = this._getSubscriberIndex(service);
-			this.subscribedServices.splice(index,1);
-			if(this.subscribedServices.length === 0 && this.running) this.stop();
-		}
-
-		// Timer management
-		start() {
-			if(this.running) this.stop();
-			this.running = true;
-			this.intervalID = setInterval(this.historicalDatas.bind(this),this.minutes * 60 * 1000);
-		}
-		stop() {
-			clearInterval(this.intervalID);
-			this.running = false;
-			this.intervalID = null;
-		}
-		
-		// Data management
-		historicalDatas() {
-            if(this.subscribedServices.length !== 0) {
-                for(let s in this.subscribedServices) {
-                    if (this.subscribedServices.hasOwnProperty(s)) {
-                        let service = this.subscribedServices[s];
-                        if(typeof(service.callback) == 'function') service.callback(service.history);
-                    }
-                }	
-            }
-			else {
-                this.callback(this.lastValue);
-			}
-		}
-		addData(data,service) {
-			var history;
-			if(this.subscribedServices.length !== 0 && service) {
-				history = this.getSubscriber(service).history;
-			}
-			else history = this.history;
-			
-			history.push(data);
-			if(this.running === false) this.start()
-		}
-		emptyData(service) {
-            var history;
-			if(this.subscribedServices.length !== 0 && service) {
-				history = this.getSubscriber(service).history;
-			}
-			else history = this.history;
-			
-			history=[];
-		}
-	}
-	
+		this.start();
 }
+	
+	// Subscription management
+timedData.prototype.subscribe=function(service,callback) {
+		if(typeof(callback) == 'function') {
+			callback = callback.bind(this);
+		}
+		let newService = {
+			'service'   : service,
+			'callback'  : callback,
+			'history'   : []
+		};
+		console.log('new service subscribed',newService);
+		this.subscribedServices.push(newService);
+		if(!this.running) this.start();
+}
+timedData.prototype.getSubscriber=function(service) {
+		let findServ = function(element) {
+			return element.service === service;
+		};
+		return this.subscribedServices.find(findServ);
+}
+timedData.prototype._getSubscriberIndex=function(service) {
+		let findServ = function(element) {
+			return element.service === service;
+		};
+		return this.subscribedServices.findIndex(findServ);
+}
+timedData.prototype.getSubscribers=function() {
+		return this.subscribedServices;
+}
+timedData.prototype.unsubscribe=function(service) {
+		console.log('service want to unsubscribe',service);
+		let index = this._getSubscriberIndex(service);
+		this.subscribedServices.splice(index,1);
+		if(this.subscribedServices.length === 0 && this.running) this.stop();
+}
+
+	// Timer management
+timedData.prototype.start=function() {
+		if(this.running) this.stop();
+		this.running = true;
+		this.intervalID = setInterval(this.historicalDatas.bind(this),this.minutes * 60 * 1000);
+		console.log('started the timer',this.intervalID,'every',this.minutes,'min');
+}
+timedData.prototype.stop=function() {
+		console.log('stopping the timer',this.intervalID);
+		clearInterval(this.intervalID);
+		this.running = false;
+		this.intervalID = null;
+}
+	
+	// Data management
+timedData.prototype.historicalDatas=function() {
+		if(this.subscribedServices.length !== 0) {
+			for(let s in this.subscribedServices) {
+				if (this.subscribedServices.hasOwnProperty(s)) {
+					let service = this.subscribedServices[s];
+					console.log('calling back for',this.IntervalID);
+					if(typeof(service.callback) == 'function') service.callback(service.history);
+				}
+			}	
+		}
+		else {
+			console.log('calling back for',this.IntervalID);
+			if(typeof(this.callback) == 'function') this.callback(this.lastValue);
+		}
+}
+timedData.prototype.addData=function(data,service) {
+		var history;
+		if(this.subscribedServices.length !== 0 && service) {
+			history = this.getSubscriber(service).history;
+		}
+		else history = this.history;
+		
+		console.log('adding data to ',this.IntervalID,' from addData',data);
+		history.push(data);
+		if(this.running === false) this.start()
+}
+timedData.prototype.emptyData=function(service) {
+		var history;
+		if(this.subscribedServices.length !== 0 && service) {
+			history = this.getSubscriber(service).history;
+		}
+		else history = this.history;
+		console.log('empty data of',this.IntervalID);
+		history=[];
+}
+
+	
