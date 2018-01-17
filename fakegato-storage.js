93e9045437c311660fd5bae8d7d0bb4ea79beb38
Nebz <28622481+NebzHB@users.noreply.github.com>
2018-01-17 10:41:04 +0100

    fs writer working

--- fakegato-storage.js
+++ fakegato-storage.js
@@ -6,24 +6,40 @@ var debug = require('debug')('FakeGatoStorage');
 var fs = require('fs');
 var os = require('os');
 
+var fileSuffix = '_persist.json';
+
 class FakeGatoStorage {
 	constructor(params) {
 		if (!params)
 			params = {};
 		
-		this.storage = params.storage || 'fs';
+		this.writers = []
 		
 		this.log = params.log || {};
 		if (!params.log || !params.log.debug) {
 			if(DEBUG) this.log.debug = console.log;
 			else this.log.debug = function(){};
 		}
+	}
+	
+	addWriter(service,params) {
+		if (!params)
+			params = {};
 		
-		switch(this.storage) {
+		this.log.debug("** Fakegato-storage AddWriter :",service.accessoryName);
+		
+		let newWriter = {
+			'service': service,
+			'callback': params.callback,
+			'storage' : params.storage || 'fs',
+			'path'    : '',
+			'storageHandler': null
+		};
+
+		switch(newWriter.storage) {
 			case 'fs' :
-				this.storageHandler = fs;
-				this.path = params.path || os.homedir()+'/.homebridge/';
-				this.log.debug('***StoragePath***',this.path);
+				newWriter.storageHandler = fs;
+				newWriter.path = params.path || os.homedir()+'/.homebridge/';
 			break;
 			case 'googleDrive' :
 			
@@ -34,15 +50,66 @@ class FakeGatoStorage {
 			break;
 			*/
 		}
-
-		
+		this.writers.push(newWriter);
+	}
+	getWriter(service) {
+		let findServ = function (element) {
+			return element.service === service;
+		};
+		return this.writers.find(findServ);
+	}
+	_getWriterIndex(service) {
+		let findServ = function (element) {
+			return element.service === service;
+		};
+		return this.writers.findIndex(findServ);
+	}
+	getWriters() {
+		return this.writers;
+	}
+	delWriter(service) {
+		let index = this._getWriterIndex(service);
+		this.writers.splice(index, 1);
 	}
 	
-	write(key,data) {
-		
+	write(params) { // must be asynchronous
+		this.log.debug("** Fakegato-storage write :",params.service.accessoryName,params.data);
+		let writer = this.getWriter(params.service);
+		switch(writer.storage) {
+			case 'fs' :
+				writer.storageHandler.writeFile(writer.path+writer.service.accessoryName+fileSuffix,params.data,'utf8',typeof(params.callback)=='function'?params.callback:(typeof(writer.callback)=='function'?writer.callback:function(){}));
+			break;
+			case 'googleDrive' :
+			
+			break;
+			/*
+			case 'memcached' :
+			
+			break;
+			*/
+		}
 	}
-	read(key){
-		
+	read(params){ // must by synchronous
+		let writer = this.getWriter(params.service);
+		switch(writer.storage) {
+			case 'fs' :
+				if(writer.storageHandler.existsSync(writer.path+params.service.accessoryName+fileSuffix)) {
+					let data = writer.storageHandler.readFileSync(writer.path+params.service.accessoryName+fileSuffix,'utf8');
+					this.log.debug("** Fakegato-storage FS read :",params.service.accessoryName,data);
+					return data;
+				} else {
+					return null;
+				}
+			break;
+			case 'googleDrive' :
+			
+			break;
+			/*
+			case 'memcached' :
+			
+			break;
+			*/
+		}
 	}
 }
 
