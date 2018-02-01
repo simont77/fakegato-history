edab7a52ab9aec0773797f80c9606117b42597a8
Nebz <28622481+NebzHB@users.noreply.github.com>
2018-02-01 11:00:14 +0100

    average might be == 0 + defer saving if saving in progress

--- fakegato-history.js
+++ fakegato-history.js
@@ -238,7 +238,7 @@ module.exports = function (pHomebridge) {
 							
 							for (let key in previousAvrg) { // each record of previous average
 								if (previousAvrg.hasOwnProperty(key) && key != 'time') { // except time
-									if(	calc.avrg[key] == 0 || // zero value
+									if(	//calc.avrg[key] == 0 || // zero value
 										calc.avrg[key] === undefined) // no key (meaning no value received for this key yet)
 									{
 										calc.avrg[key]=previousAvrg[key];
@@ -287,7 +287,7 @@ module.exports = function (pHomebridge) {
 							
 							for (let key in previousAvrg) { // each record of previous average
 								if (previousAvrg.hasOwnProperty(key) && key != 'time') { // except time
-									if(	calc.avrg[key] == 0 || // zero value
+									if(	//calc.avrg[key] == 0 || // zero value
 										calc.avrg[key] === undefined) // no key (meaning no value received for this key yet)
 									{
 										calc.avrg[key]=previousAvrg[key];
@@ -336,7 +336,7 @@ module.exports = function (pHomebridge) {
 							
 							for (let key in previousAvrg) { // each record of previous average
 								if (previousAvrg.hasOwnProperty(key) && key != 'time') { // except time
-									if(	calc.avrg[key] == 0 || // zero value
+									if(	//calc.avrg[key] == 0 || // zero value
 										calc.avrg[key] === undefined) // no key (meaning no value received for this key yet)
 									{
 										calc.avrg[key]=previousAvrg[key];
@@ -423,6 +423,8 @@ module.exports = function (pHomebridge) {
 			this.memoryAddress = 0;
 			this.dataStream = '';
 
+			this.saving=false;
+			
 			this.registerEvents();
 			if(this.storage === undefined) {
 				this.loaded=true;
@@ -575,20 +577,30 @@ module.exports = function (pHomebridge) {
 		}
 		save() {
 			if(this.loaded) {
-				let data = {
-						firstEntry:this.firstEntry,
-						lastEntry :this.lastEntry,
-						usedMemory:this.usedMemory,
-						refTime   :this.refTime,
-						initialTime:this.initialTime,
-						history   :this.history
-					};
-			
-			
-				homebridge.globalFakeGatoStorage.write({
-					service: this,
-					data:typeof(data) === "object" ? JSON.stringify(data) : data
-				});
+				if(!this.saving) {
+					this.saving=true;
+					let data = {
+							firstEntry:this.firstEntry,
+							lastEntry :this.lastEntry,
+							usedMemory:this.usedMemory,
+							refTime   :this.refTime,
+							initialTime:this.initialTime,
+							history   :this.history
+						};
+				
+				
+					homebridge.globalFakeGatoStorage.write({
+						service: this,
+						data:typeof(data) === "object" ? JSON.stringify(data) : data,
+						callback:function(){
+							this.saving=false;
+						}.bind(this)
+					});
+				} else {
+					setTimeout(function(){ // retry in 100ms
+						this.save();
+					}.bind(this),100);	
+				}
 			} else {
 				setTimeout(function(){ // retry in 100ms
 					this.save();
